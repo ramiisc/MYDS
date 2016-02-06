5fe4e2e3472d61b992b46fd0909ee16d66f90a56
rk1 <prk.iisc@gmail.com>
2016-02-06 15:57:28 +0530

    solved remove duplicates in sorted array with o(n)

--- src/arrays/RemoveDuplicates.java
+++ src/arrays/RemoveDuplicates.java
@@ -16,39 +16,23 @@ public class RemoveDuplicates {
 
 	}
 
-	public static void helper2(int num[], int index, int count) {
-		index  = index - count + 1;
-		while (index < num.length - count && num[index] != '\0') {
-			num[index] = num[index + count];
-			index++;
-		}
-		int i = 0;
-		while (i < count) {
-			num[index - i] = '\0';
+	public static void removeDup2(int num[]) {
+		int unique = 0;
+		for (int i = 1; i < num.length; i++) {
+			if(num[unique] != num[i]) {
+				num[unique+1] = num[i];
+				unique++;
+			}
 			i++;
 		}
-		
-
-	}
-
-	public static void removeDup2(int num[]) {
-		int i = 0, count = 0;
-		boolean found = false;
-		while (i < num.length - 1 && num[i] != '\0') {
-			if (num[i] == num[i + 1]) {
-				found = true;
-				count++;
-				i++;
-			} else if (found == true) {
-				helper2(num, i, count);
-				found = false;
-				i = i - count + 1;
-				count = 0;
-
-			} else {
-				i++;
-			}
+		unique++;
+		System.out.println("unique:" + unique);
+		while(unique <  num.length) {
+			num[unique] = 0;
+			unique++;
 		}
+		
+		
 	}
 
 	public static void removeDup1(int num[]) {
@@ -63,10 +47,9 @@ public class RemoveDuplicates {
 	}
 
 	public static void main(String args[]) {
-		int[] num = { 1, 2, 3, 3, 3, 4, 4, 5, 5, 6 };
+		int[] num = { 1, 1, 2, 3, 3, 4, 5, 5, 5, 6 };
 		System.out.println("original length:" + num.length);
 		removeDup2(num);
-		System.out.println("new length:" + num.length);
 		for (int i = 0; i < num.length; i++) {
 			System.out.print(" " + num[i]);
 		}
